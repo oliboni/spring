17d38e97504579f97b207436c41166b91f617941
Rodrigo Oliboni <rodrigo_oliboni@hotmail.com>
2021-01-15 14:35:06 -0300

    Paginacao

--- forum/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,6 +1,8 @@
 package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +11,7 @@ import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable pageable);
     //Caso exista ambiguidade no nome pode usar assim para pegar dentro do relacimanto curso
     //List<Topico> findByCurso_Nome(String nomeCurso);
 
